11789b411105b8989b650fe40c2992cfeea1e3e2
anRan <1299115602@qq.com>
2021-01-10 13:37:19 +0800

    #

--- app/spider/yushu_book.py
+++ app/spider/yushu_book.py
@@ -34,6 +34,7 @@ class YuShuBook:
     def calculate_start(self, page):
         return (page - 1) * current_app.config['PER_PAGE']
 
+    # 使用property修饰first()方法，使得该方法变成了first属性的getter方法
     @property
     def first(self):
         return self.books[0] if self.total >= 1 else None
